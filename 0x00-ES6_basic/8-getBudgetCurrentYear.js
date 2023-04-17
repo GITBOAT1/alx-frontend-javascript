46a7c2427188b304d1dd9708a50e6f01f2a62cc0
Joe <boat232001@yahoo.com>
2023-04-18 07:10:12 +1000

    cumputer property

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -12,4 +12,4 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
 
   return budget;
 }
-With this modification, the properties of the budget object are defined using computed property names, which allows us to use an expression to compute the property name. In this case, we're using the getCurrentYear function to compute the year and include it in the property name. This makes the code more concise and easier to read.
+
