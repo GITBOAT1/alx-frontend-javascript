63d7ebc99da49dec856586f16387a516f571596d
Joe <boat232001@yahoo.com>
2023-04-20 05:15:56 +1000

    1 promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,19 +2,12 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     const error = 'The fake API is not working currently';
     if (success) {
-      const response = {
+      const respons = {
         status: 200,
         body: 'Success',
       };
-      resolve(response);
-    } else {
-      reject(error);
+      resolve(respons);
     }
-  }).then((response) => {
-    console.log(response);
-    // Handle successful response here
-  }).catch((error) => {
-    console.error(error);
-    // Handle error response here
+    reject(error);
   });
 }
