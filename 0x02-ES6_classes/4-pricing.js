729648763d0c81ee66e1191ae8edcf39761a934b
Joe <boat232001@yahoo.com>
2023-04-23 21:08:57 +1000

    static pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+import Currency from './3-currency';
+
+export default class Pricing extends Currency {
+  constructor(amount, currency) {
+    super();
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amt) {
+    this._amount = amt;
+  }
+
+  // getter and setter for currency
+  get currency() {
+    return this.currency;
+  }
+
+  set currency(cur) {
+    this._currency = cur;
+  }
+
+  /* Implement a method named displayFullPrice that
+  returns the attributes in the following format */
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
