700163ace425c3b330ecf89042cd7e45d78341a8
Joe <boat232001@yahoo.com>
2023-04-25 21:04:17 +1000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
   let result = '';
-  Set.forEach((val) => {
+  for (const val of set) {
     if (val.startsWith(startString)) {
-      result += `${val.substring(startString.length)} + -`;
+      result += `${val.slice(startString.length)}-`;
     }
-  });
+  }
   return result.slice(0, -1);
 }
