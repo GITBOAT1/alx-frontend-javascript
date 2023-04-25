96b79bce67b0a226510996e46648d4379ce4d9b4
Joe <boat232001@yahoo.com>
2023-04-25 21:14:24 +1000

    Map

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,8 @@
-export default function hasValuesFromArray(val1, val2) {
-  return val1.has(val2);
+export default function hasValuesFromArray(set, arr) {
+  for (const val of arr) {
+    if (!set.has(val)) {
+      return false;
+    }
+  }
+  return true;
 }
