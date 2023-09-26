7cf1e020e338819888cfee9fe047ea9e69d7406b
Joe <boat232001@yahoo.com>
2023-09-27 06:59:38 +1000

    report

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,18 +1,12 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const employee of employeesList) {
-    const department = employee.department;
-    if (!allEmployees[department]) {
-      allEmployees[department] = [];
-    }
-    allEmployees[department].push(employee.name);
-  }
-
-  return {
-    allEmployees,
-    getNumberOfDepartments: function() {
-      return Object.keys(allEmployees).length;
+  const reportObject = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     },
   };
+
+  return reportObject;
 }
