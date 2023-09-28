df90800c826259ea948e863074823fa56fa07453
Joe <boat232001@yahoo.com>
2023-09-28 21:23:56 +1000

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,21 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
+
+const log = 'Signup system offline';
+
+const uploadPhotePromise = new Promise((reslove) => {
+  reslove(uploadPhoto);
+});
+
+const createUserPromise = new Promise((resolve) => {
+  resolve(createUser);
+});
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhotePromise, createUserPromise])
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
+    })
+    .catch(() => {
+      console.log(log);
+    });
+}
